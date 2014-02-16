5f176e0e71bc275117aecabeaacbbaf5d008aeb4
jalcim <jalcim@student.42.fr>
2014-02-16 12:44:55 +0100

    test1

---
+++ main.c
@@ -0,0 +1,55 @@
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+
+#include <unistd.h>
+#include <sys/mman.h>
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int shmid;
+	int key;
+	char *addr;
+	struct shmid_ds seg_info;
+
+	key = 75;
+	// 1 page == PAGE_SIZE
+	if ((shmid = shmget(IPC_PRIVATE, 1023, IPC_CREAT | IPC_EXCL)) == -1)//creation segment memoire et retourd d'un id segment
+		perror("shmid non valide -> ");
+
+	addr = shmat(shmid, 0, 0);//attachement segment -> processus 
+//(si addr(arg2) != 0 && arg3(flag) == SHM_RMD ? attachement a l'addresse (SHMLBA * ((addr / SHMLBA) + SHMLBA)) : addr -> ? -> system => ! => addr
+	if (-1 == (long unsigned int)addr)
+		perror("error -> shmat : ");
+
+	if (errno == EACCES)
+		printf("EACCES\n");
+	else if (errno == EINVAL)
+		printf("EINVAL\n");
+	else if (errno == ENOMEM)
+		printf("ENOMEM\n");
+
+	if ((shmctl(shmid, IPC_STAT, &seg_info)) == -1)//recuperation des info_sig 
+		perror("error -> shmctl : ");
+
+	system("ipcs");
+
+	if ((shmdt(addr)) == -1)//detachement segment -> processus
+		perror("error -> shmdt : ");
+
+	if ((shmctl(shmid, IPC_RMID, &seg_info)) == -1)//marquage du segment pour destruction (quand file vide) shm_nattch shmid_ds /!\ uniquement createur ou proprietaire (ou setuid)
+		perror("error -> shmctl : ");
+
+	seg_info.shm_nattch = 0;//fille mise a 0 pour destruction effectif du segment memoire
+	if ((shmctl(shmid, IPC_SET, &seg_info)) == -1)//application des modification au segment
+		perror("error -> shmctl : ");
+
+	return (errno);
+}
+
+
+//	mem = mmap(NULL, 60, PROT_WRITE | PROT_READ, fd, 0);
